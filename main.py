f7fe674dc093636564a0203d5cec8d3a08ff2084
NoahSchuldt <noahglosan@icloud.com>
2024-10-03 16:07:15 +0200

    Update main.py
    Kreis Flächeninhalt hinzugefügt

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import math
 
 def pythagoras(a, b):
     return math.sqrt(a**2 + b**2)
-
-# Eingabe und Berechnung
+    
 a = float(input("Gib a ein: "))
 b = float(input("Gib b ein: "))
 print("Hypotenuse c:", pythagoras(a, b))
-input('Drücke Enter zum Beenden')
+
+def flaeche_kreis(r)
+    return math.pi * r**2
+
+r = float(input('Gib den Radius des Kreises ein: ')
+print('Der Flächeninhalt des Kreises ist:' flaeche_kreis(r))
