9082fd77bc5552b747867e4aa070a1e680907586
NoahSchuldt <noahglosan@icloud.com>
2024-10-03 10:27:32 +0200

    0.1
    added Pythagoras

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+import math
+def pythagoras(a,b):
+  return math.sqrt(a**2 + b**2)
+
+a=float(input('Gib a ein: '))
+b=float(input('Gib b ein: '))
+print('Hypotenuse C: 'pythagoras(a,b))
