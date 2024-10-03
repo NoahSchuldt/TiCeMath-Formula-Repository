9af3c9e07fa602918fe6865d3adcdc2c0a4e6713
NoahSchuldt <noahglosan@icloud.com>
2024-10-03 19:41:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import math
 
 def pythagoras(a, b):
-    return math.sqrt(a**2 + b**2)
-    
+    return math.sqrt(a**2 + b**2)   
 a = float(input("Gib a ein: "))
 b = float(input("Gib b ein: "))
 print("Hypotenuse c:", pythagoras(a, b))
 
 def flaeche_kreis(r):
     return math.pi * r**2
-
 r = float(input('Gib den Radius des Kreises ein: '))
 print('Der Flächeninhalt des Kreises ist:', flaeche_kreis(r))
 
 def umfang_kreis(rU):
-    return 2 + math.pi * rU
+    return 2 + math.pi * kU
+kU = float(input('Gib den Radius ein: '))
+print('Der Umfang ist: ', umfang_kreis(kU))
 
-rU = float(input('Gib den Radius ein: '))
-print('Der Umfang ist: ', umfang_kreis(rU))
-
+def zylinder_volumen(rZ: float, h: float):
+    return math.pi * (rZ * rZ) * h
+rZ = float(input('Gib den Radius des Zylinders ein: '))
+h= float(input('Gib die Höhe des Zylinder an: '))
+print('Der Zylinder hat ein Volumen von: ', zylinder_volumen(rZ, h))
 
 input()
