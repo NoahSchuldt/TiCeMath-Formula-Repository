c4d20f0110826f4bc78337b78d102127326f3d7c
DeAnnabellezockt <weisannabelle@gmail.com>
2024-10-08 15:11:20 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,45 @@
 import math
 
+def therme_und_gleichungen():
+    print("therme_und_gleichungen")
+
+def funktionen():
+    print("funktionen")
+
+def geometrie():
+    print("geometrie")
+
+def wahrscheinlichkeiten():
+    print("wahrscheinlichkeiten")
+
+def obermenü():
+    while True:
+        print("\nObermenü:")
+        print("1. therme_und_gleichungen 1")
+        print("2. fuktionen 2")
+        print("3. geometrie 3")
+        print("4. wahrscheinlichkeiten 4")
+
+auswahl = input("Bitte wählen")
+
+if auswahl == '1':
+    therme_und_gleichungen()
+
+elif auswahl == '2':
+    funktionen()
+
+elif auswahl == '3':
+    geometrie()
+
+elif auswahl == '4':
+    wahrscheinlichkeiten()
+
+else:
+    print:("ungültige auswahl")
+
+if __name__ = "__main__":
+    obermenü()
+
 def pythagoras():
     a = float(input("Gib a ein(cm): "))
     b = float(input("Gib b ein(cm): "))
