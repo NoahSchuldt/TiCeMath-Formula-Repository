baa9bfed8f66a6b6bc60f1a75d12d7664b48e508
NoahSchuldt <noahglosan@icloud.com>
2024-10-15 14:23:33 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -40,22 +40,24 @@ def kegel_oberfläche():
     rK = float(input('Gib den Radius der Grundfläche des Kegels an: '))
     sK = float(input('Gib die Seitenlänge des Kegels an: '))
     return rK * math.pi * (rK * sK)
+over = int(input('[1]Menü1\n[2]Menü2'))
+if over==1:
+    print ("[1]Hypotenuse\n[2]Kreisfläche\n[3]Kreisumfang\n[4]Zylindervolumen\n[5]Distanzumrechnungen\n[6]Kegeloberfläche\n[0]Programm beeenden")
 
-#Gibt die Optionen weiter an den User
-print ("[1]Hypotenuse\n[2]Kreisfläche\n[3]Kreisumfang\n[4]Zylindervolumen\n[5]Distanzumrechnungen\n[6]Kegeloberfläche\n[0]Programm beeenden")
-
-answer = int(input("Welche Option?\n"))
-if answer==1:
-    print("Hypotenuse c(cm):", pythagoras())
-elif answer==2:
-    print('Der Flächeninhalt des Kreises ist(cm²):', flaeche_kreis())
-elif answer==3:
-    print('Der Umfang ist(cm): ', umfang_kreis())
-elif answer==4:
-    print('Der Zylinder hat ein Volumen von(cm³): ', zylinder_volumen())
-elif answer==5:
-    Weg()
-elif answer==6:
-    print('Die Oberfläche des Kegels ist: ', kegel_oberfläche())
-elif answer==0:
-    QUIT
+    answer = int(input("Welche Option?\n"))
+    if answer==1:
+        print("Hypotenuse c(cm):", pythagoras())
+    elif answer==2:
+        print('Der Flächeninhalt des Kreises ist(cm²):', flaeche_kreis())
+    elif answer==3:
+        print('Der Umfang ist(cm): ', umfang_kreis())
+    elif answer==4:
+        print('Der Zylinder hat ein Volumen von(cm³): ', zylinder_volumen())
+    elif answer==5:
+        Weg()
+    elif answer==6:
+        print('Die Oberfläche des Kegels ist: ', kegel_oberfläche())
+    elif answer==0:
+        print()
+elif over==2:
+    print()
